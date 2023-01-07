b29d3da4e98882bd1a04316a7a94aae4a8480aa1
ERwiin 21 MP <76853291+ERwiin21MP@users.noreply.github.com>
2023-01-07 14:13:08 -0600

    Add files via upload

---
+++ example_printTable.py
@@ -0,0 +1,14 @@
+from ERlib import printTable
+
+TituloDeLaTabla = "Los equipos con más Champions League"
+
+Tabla = [
+    ["Posición", "Equipo", "Títulos"],
+    ["1°", "Real Madrid Club de Fútbol", 14],
+    ["2°", "Associazione Calcio Milán", 7],
+    ["3°", "Bayern de Múnich", 6],
+    ["4°", "Liverpool Fútbol Club", 6],
+    ["5°", "Fútbol Club Barcelona", 5]
+]
+
+printTable(Tabla, TituloDeLaTabla)
